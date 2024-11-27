61404f6369ae02b9720103951e78a43df120d7a4
aaron.shoal <aaron.shoal@servicenow.com>
2024-11-27 07:24:38 -0800

    More updates

--- generate-commit-deltas.py
+++ generate-commit-deltas.py
@@ -42,6 +42,7 @@ def get_commit_info(repo_path, old_hash, new_hash):
         'git', '-C', repo_path, 'log',
         '--pretty=format:%H|%an|%ad|%s',
         '--date=iso',
+        '--no-merges',
         f'{old_hash}..{new_hash}'
     ]
     result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
